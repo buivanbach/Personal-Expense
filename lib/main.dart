826eeb1513d63c4eb3767689fe26e5497e1bf17d
buibach27 <buibach27@gmail.com>
2022-10-18 17:12:48 +0700

    C2 : Readme update shortScreen

--- lib/main.dart
+++ lib/main.dart
@@ -69,7 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Personal Expenses"),
+        title: Text("Personal Expenses  "),
         actions: <Widget>[
           IconButton(
             onPressed: () => _startAddNewTransaction(context),
